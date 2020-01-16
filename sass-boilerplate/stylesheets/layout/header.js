0de9c42854e51e9e78b705e128bfad5ccff21e1a
Ira-drozd <moby.dick.killer@gmail.com>
2020-01-16 17:58:45 +0300

    add right header and cards

--- sass-boilerplate/stylesheets/layout/header.js
+++ sass-boilerplate/stylesheets/layout/header.js
@@ -1,5 +1,9 @@
-/*
-(function() {
+(function () {
+    const burger=document.querySelector(".burger");
+    const menu=document.querySelector(".mobile-menu");
+    burger.addEventListener("click", function () {
+        //burger.classList.toggle("burger-active");
+        menu.classList.toggle("open-menu");
+    });
+}());
 
-    alert("eee");
-}());*/
