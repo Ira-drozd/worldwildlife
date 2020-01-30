d9ca6c9c262a10d6f7ff65a001b65898f92535ff
Ira <moby.dick.killer@gmail.com>
2020-01-30 22:25:19 +0300

    incorrect structure

--- sass-boilerplate/stylesheets/pages/worldwildlifearctic.js
+++ sass-boilerplate/stylesheets/pages/worldwildlifearctic.js
@@ -1,3 +1,30 @@
+
+(function () {
+
+     let list = document.querySelector(".circle-card-species");//карусель
+     let widthCarusel = document.querySelector(".circle-card-species").offsetWidth;//467
+
+     let widthImg = list.querySelector(".circle-card-item").offsetWidth;//150
+     let count = Math.floor(widthCarusel / widthImg);//3
+     let listElems = list.querySelectorAll(".circle-card-item");//13
+     let carusel = list.querySelector(".circle-card-pillars");//что двигаем
+     let position = 0;
+
+     list.querySelector('.prev').onclick = function () {
+         position += widthImg * count - widthImg;
+         position = Math.min(position, 0);
+         carusel.style.marginLeft = position + 'px';
+     };
+
+     list.querySelector('.next').onclick = function () {
+         position -= widthImg * count - widthImg;
+         position = Math.max(position, -widthImg * (listElems.length - count) - widthImg * 2);
+         carusel.style.marginLeft = position + 'px';
+     };
+
+}());
+
+
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
@@ -6,4 +33,5 @@ function myFunction() {
     let scrolled = (winScroll / height) * 100;
     document.getElementById("myBar").style.width = scrolled + "%";
 }
+
 
